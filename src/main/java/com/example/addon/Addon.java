5738ca1ad9053805ceb9192377071f85daaefbfe
KassuK1 <kasper.s.kinnunen@gmail.com>
2022-10-15 20:16:38 +0300

    idiootti

--- src/main/java/com/example/addon/Addon.java
+++ src/main/java/com/example/addon/Addon.java
@@ -3,7 +3,7 @@ package com.example.addon;
 import com.example.addon.commands.CommandExample;
 import com.example.addon.hud.HudExample;
 import com.example.addon.modules.GhostCrystal;
-import com.example.addon.modules.ModuleExample;
+import com.example.addon.modules.LegitSpeed;
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.commands.Commands;
@@ -15,15 +15,15 @@ import org.slf4j.Logger;
 
 public class Addon extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
-    public static final Category CATEGORY = new Category("Example");
-    public static final HudGroup HUD_GROUP = new HudGroup("Example");
+    public static final Category CATEGORY = new Category("Blackout");
+    public static final HudGroup HUD_GROUP = new HudGroup("Blackout");
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing Meteor Addon Template");
+        LOG.info("Initializing Blackout");
 
         // Modules
-        Modules.get().add(new ModuleExample());
+        Modules.get().add(new LegitSpeed());
         Modules.get().add(new GhostCrystal());
 
         // Commands
