88a0ff3501ac978863849f0e69fdb1abcb998905
OLEPOSSU <atska07@gmail.com>
2022-10-15 21:13:00 +0300

    kyl

--- src/main/java/com/example/addon/Addon.java
+++ src/main/java/com/example/addon/Addon.java
@@ -3,7 +3,6 @@ package com.example.addon;
 import com.example.addon.commands.CommandExample;
 import com.example.addon.hud.HudExample;
 import com.example.addon.modules.GhostCrystal;
-import com.example.addon.modules.LegitSpeed;
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.commands.Commands;
@@ -23,7 +22,6 @@ public class Addon extends MeteorAddon {
         LOG.info("Initializing Blackout");
 
         // Modules
-        Modules.get().add(new LegitSpeed());
         Modules.get().add(new GhostCrystal());
 
         // Commands
