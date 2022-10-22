dddbd4e7fae9eee9d5944cacd761d890930c4e16
KassuK1 <kasper.s.kinnunen@gmail.com>
2022-10-22 12:27:18 +0300

    Korjasin paskan sprinttis opettele koodaa

--- src/main/java/com/example/addon/modules/anarchy/Sprint.java
+++ src/main/java/com/example/addon/modules/anarchy/Sprint.java
@@ -2,11 +2,22 @@ package com.example.addon.modules.anarchy;
 
 import com.example.addon.Addon;
 import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.settings.EnumSetting;
+import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.player.PlayerUtils;
 import meteordevelopment.orbit.EventHandler;
 import meteordevelopment.orbit.EventPriority;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 
 public class Sprint extends Module {
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+    public final Setting<SprintMode> sprintmode = sgGeneral.add(new EnumSetting.Builder<SprintMode>()
+        .name("mode")
+        .description("The method of sprinting.")
+        .defaultValue(SprintMode.Vanilla)
+        .build()
+    );
 
     public Sprint() {
         super(Addon.ANARCHY, "Sprint", "Non shit sprint");
@@ -15,8 +26,23 @@ public class Sprint extends Module {
     @EventHandler(priority = EventPriority.HIGHEST)
     private void onTick(TickEvent.Pre event) {
         if (mc.player != null && mc.world != null) {
-            mc.player.setSprinting(true);
-            mc.player.ticksSinceSprintingChanged = 100;
+            switch (sprintmode.get()){
+                case Vanilla -> {
+                    if (mc.options.forwardKey.isPressed())
+                        mc.player.setSprinting(true);
+                }
+                case Omni -> {
+                    if (PlayerUtils.isMoving())
+                        mc.player.setSprinting(true);
+                }
+                case Rage ->
+                    mc.player.setSprinting(true);
+            }
         }
+    }
+    public enum SprintMode {
+        Vanilla,
+        Omni,
+        Rage
     }
 }
