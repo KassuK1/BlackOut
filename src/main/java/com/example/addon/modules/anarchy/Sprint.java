15bb3c104539cd138a74ac39c0b09e03d10f5613
KassuK1 <kasper.s.kinnunen@gmail.com>
2022-10-22 12:44:57 +0300

    Useless addition to sprint because rak is fucking autistic

--- src/main/java/com/example/addon/modules/anarchy/Sprint.java
+++ src/main/java/com/example/addon/modules/anarchy/Sprint.java
@@ -2,6 +2,7 @@ package com.example.addon.modules.anarchy;
 
 import com.example.addon.Addon;
 import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.settings.BoolSetting;
 import meteordevelopment.meteorclient.settings.EnumSetting;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.systems.modules.Module;
@@ -12,6 +13,12 @@ import meteordevelopment.meteorclient.settings.SettingGroup;
 
 public class Sprint extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
+    private final Setting<Boolean> sprintTime = sgGeneral.add(new BoolSetting.Builder()
+        .name("sprintTime")
+        .description("Rak is autistic and thinks this is needed")
+        .defaultValue(true)
+        .build()
+    );
     public final Setting<SprintMode> sprintmode = sgGeneral.add(new EnumSetting.Builder<SprintMode>()
         .name("mode")
         .description("The method of sprinting.")
@@ -28,15 +35,21 @@ public class Sprint extends Module {
         if (mc.player != null && mc.world != null) {
             switch (sprintmode.get()){
                 case Vanilla -> {
-                    if (mc.options.forwardKey.isPressed())
+                    if (mc.options.forwardKey.isPressed()){
                         mc.player.setSprinting(true);
+                        if (sprintTime.get())
+                            mc.player.ticksSinceSprintingChanged = 100;}
                 }
                 case Omni -> {
-                    if (PlayerUtils.isMoving())
+                    if (PlayerUtils.isMoving()){
                         mc.player.setSprinting(true);
+                        if (sprintTime.get())
+                            mc.player.ticksSinceSprintingChanged = 100;}
                 }
-                case Rage ->
+                case Rage ->{
                     mc.player.setSprinting(true);
+                    if (sprintTime.get())
+                        mc.player.ticksSinceSprintingChanged = 100;}
             }
         }
     }
