5615ad7b24f0d54b1cdd37b85879f5e42d53fc6d
KassuK1 <kasper.s.kinnunen@gmail.com>
2022-11-06 13:04:07 +0200

    noni rotationin mutta ei sitä vammasta autoyeettia koska kukaan ei käytä muut ku vammaset

--- src/main/java/com/example/addon/modules/anarchy/FastXP.java
+++ src/main/java/com/example/addon/modules/anarchy/FastXP.java
@@ -2,14 +2,13 @@ package com.example.addon.modules.anarchy;
 
 import com.example.addon.Addon;
 import meteordevelopment.meteorclient.events.world.TickEvent;
-import meteordevelopment.meteorclient.settings.BoolSetting;
-import meteordevelopment.meteorclient.settings.IntSetting;
-import meteordevelopment.meteorclient.settings.Setting;
-import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.player.Rotations;
 import meteordevelopment.orbit.EventHandler;
 import meteordevelopment.orbit.EventPriority;
 import meteordevelopment.meteorclient.mixin.MinecraftClientAccessor;
+import net.minecraft.item.Items;
 
 /*
 Made by KassuK
@@ -17,6 +16,12 @@ Made by KassuK
 
 public class FastXP extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
+    private final Setting<rotationmode> rotmode = sgGeneral.add(new EnumSetting.Builder<rotationmode>()
+        .name("Rotation mode")
+        .description("ken i put mi balls in yo jawzz")
+        .defaultValue(rotationmode.Silent)
+        .build()
+    );
     private final Setting<Integer> YeetDelay = sgGeneral.add(new IntSetting.Builder()
         .name("YeetDelay")
         .description("Delay of yeeting")
@@ -30,7 +35,7 @@ public class FastXP extends Module {
         .name("Pitch")
         .description("Where to set Pitch")
         .defaultValue(45)
-        .range(20, 90)
+        .range(0, 90)
         .sliderMax(90)
         .build()
     );
@@ -41,6 +46,10 @@ public class FastXP extends Module {
         .defaultValue(true)
         .build()
     );
+    public enum rotationmode {
+        Silent,
+        Vanilla,
+    }
 
     public FastXP() {
         super(Addon.ANARCHY, "FastXP", "XP spamming moment");
@@ -51,7 +60,11 @@ public class FastXP extends Module {
         if (mc.player != null && mc.world != null) {
             int Ticks = Math.min(((MinecraftClientAccessor) mc).getItemUseCooldown(), YeetDelay.get());
             ((MinecraftClientAccessor) mc).setItemUseCooldown(Ticks);
-            if (Rotate.get())
-                mc.player.setPitch(Pitch.get());
+            if (mc.player.getMainHandStack().getItem() == Items.EXPERIENCE_BOTTLE  && mc.options.useKey.isPressed()){
+                if (rotmode.get().equals(rotationmode.Silent) && Rotate.get()){
+                    Rotations.rotate(mc.player.getYaw(),Pitch.get());
+                }
+                if (rotmode.get().equals(rotationmode.Vanilla) && Rotate.get())
+                    mc.player.setPitch(Pitch.get());}
     }
 }}
