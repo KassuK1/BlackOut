e36a9ae258f618be1fd7f5c2441a0d413767712f
OLEPOSSU <atska07@gmail.com>
2022-12-25 14:09:10 +0200

    mixin

--- src/main/java/kassuk/addon/blackout/BlackOut.java
+++ src/main/java/kassuk/addon/blackout/BlackOut.java
@@ -12,6 +12,8 @@ import kassuk.addon.blackout.hud.HudWaterMark;
 import kassuk.addon.blackout.managers.Managers;
 import kassuk.addon.blackout.modules.anarchy.*;
 import kassuk.addon.blackout.modules.ghost.*;
+import meteordevelopment.meteorclient.MeteorClient;
+import meteordevelopment.meteorclient.MixinPlugin;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.commands.Commands;
 import meteordevelopment.meteorclient.systems.hud.Hud;
@@ -55,6 +57,7 @@ public class BlackOut extends MeteorAddon {
         Modules.get().add(new ButtonAura());
         Modules.get().add(new CevBreaker());
         Modules.get().add(new CrystalBait());
+        Modules.get().add(new ESPPlus());
         Modules.get().add(new FastXP());
         Modules.get().add(new FeetESP());
         Modules.get().add(new FlightPlus());
---
+++ src/main/java/kassuk/addon/blackout/mixins/MixinLivingEntityRenderer.java
@@ -0,0 +1,41 @@
+package kassuk.addon.blackout.mixins;
+
+import com.mojang.blaze3d.platform.GlStateManager;
+import kassuk.addon.blackout.modules.anarchy.ESPPlus;
+import meteordevelopment.meteorclient.renderer.Renderer3D;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import meteordevelopment.meteorclient.utils.player.ChatUtils;
+import net.minecraft.client.render.*;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.client.render.entity.model.EntityModel;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+import static org.lwjgl.opengl.GL11.*;
+import static meteordevelopment.meteorclient.MeteorClient.mc;
+
+@Mixin(LivingEntityRenderer.class)
+public abstract class MixinLivingEntityRenderer<T extends LivingEntity, M extends EntityModel<T>> {
+
+    @Inject(method = "render(Lnet/minecraft/entity/LivingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At("HEAD"), cancellable = true)
+    private void renderHead(T livingEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
+        ESPPlus espPlus = Modules.get().get(ESPPlus.class);
+
+        if (livingEntity instanceof PlayerEntity && espPlus.isActive() && espPlus.players.get() && espPlus.renderer != null && matrixStack != null && !matrixStack.isEmpty() && mc.player != null) {
+            espPlus.matrixStack = matrixStack;
+        }
+    }
+
+    @Inject(method = "render(Lnet/minecraft/entity/LivingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At("TAIL"))
+    private void renderTail(T livingEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
+
+    }
+
+
+}
