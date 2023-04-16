63d5258bf66df452cd4b9c7e4eb8ad347c5ed672
tympanicblock61 <99092380+tympanicblock61@users.noreply.github.com>
2023-04-16 05:33:32 -0400

    Update SprintPlus.java

--- src/main/java/kassuk/addon/blackout/modules/SprintPlus.java
+++ src/main/java/kassuk/addon/blackout/modules/SprintPlus.java
@@ -3,11 +3,9 @@ package kassuk.addon.blackout.modules;
 import kassuk.addon.blackout.BlackOut;
 import kassuk.addon.blackout.BlackOutModule;
 import meteordevelopment.meteorclient.events.world.TickEvent;
-import meteordevelopment.meteorclient.settings.BoolSetting;
 import meteordevelopment.meteorclient.settings.EnumSetting;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
-import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.utils.player.PlayerUtils;
 import meteordevelopment.orbit.EventHandler;
 import meteordevelopment.orbit.EventPriority;
@@ -21,12 +19,6 @@ public class SprintPlus extends BlackOutModule {
         super(BlackOut.BLACKOUT, "Sprint+", "Non shit sprint");
     }
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
-    private final Setting<Boolean> sprintTime = sgGeneral.add(new BoolSetting.Builder()
-        .name("sprintTime")
-        .description("Rak is autistic and thinks this is needed")
-        .defaultValue(true)
-        .build()
-    );
     public final Setting<SprintMode> sprintmode = sgGeneral.add(new EnumSetting.Builder<SprintMode>()
         .name("mode")
         .description("The method of sprinting.")
@@ -38,22 +30,9 @@ public class SprintPlus extends BlackOutModule {
     private void onTick(TickEvent.Pre event) {
         if (mc.player != null && mc.world != null) {
             switch (sprintmode.get()){
-                case Vanilla -> {
-                    if (mc.options.forwardKey.isPressed()){
-                        mc.player.setSprinting(true);
-                        if (sprintTime.get())
-                            mc.player.ticksSinceSprintingChanged = 100;}
-                }
-                case Omni -> {
-                    if (PlayerUtils.isMoving()){
-                        mc.player.setSprinting(true);
-                        if (sprintTime.get())
-                            mc.player.ticksSinceSprintingChanged = 100;}
-                }
-                case Rage ->{
-                    mc.player.setSprinting(true);
-                    if (sprintTime.get())
-                        mc.player.ticksSinceSprintingChanged = 100;}
+                case Vanilla -> {if (mc.options.forwardKey.isPressed())mc.player.setSprinting(true);}
+                case Omni -> {if (PlayerUtils.isMoving()) mc.player.setSprinting(true);}
+                case Rage -> mc.player.setSprinting(true);
             }
         }
     }
