71a1372831d3f0cbe5a7868b548df1bce7c280e0
OLEPOSSU <atska07@gmail.com>
2023-05-06 02:11:32 +0300

    forgot the offhand

--- src/main/java/kassuk/addon/blackout/modules/OffHandPlus.java
+++ src/main/java/kassuk/addon/blackout/modules/OffHandPlus.java
@@ -3,21 +3,22 @@ package kassuk.addon.blackout.modules;
 import kassuk.addon.blackout.BlackOut;
 import kassuk.addon.blackout.BlackOutModule;
 import kassuk.addon.blackout.utils.OLEPOSSUtils;
+import meteordevelopment.meteorclient.events.packets.PacketEvent;
 import meteordevelopment.meteorclient.events.render.Render3DEvent;
 import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Modules;
-import meteordevelopment.meteorclient.utils.player.FindItemResult;
+import meteordevelopment.meteorclient.systems.modules.combat.CrystalAura;
 import meteordevelopment.meteorclient.utils.player.InvUtils;
 import meteordevelopment.meteorclient.utils.player.PlayerUtils;
 import meteordevelopment.orbit.EventHandler;
 import meteordevelopment.orbit.EventPriority;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
-import net.minecraft.item.Item;
-import net.minecraft.item.Items;
+import net.minecraft.item.*;
+
+import java.util.function.Predicate;
 
 /**
  * @author OLEPOSSU
- * @author KassuK
  */
 public class OffHandPlus extends BlackOutModule {
     public OffHandPlus() {
@@ -25,151 +26,241 @@ public class OffHandPlus extends BlackOutModule {
     }
 
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
+    private final SettingGroup sgHealth = settings.getDefaultGroup();
 
+    //   General Page
     private final Setting<Boolean> onlyInInv = sgGeneral.add(new BoolSetting.Builder()
         .name("Only in inventory")
         .description("Will only switch if you are in your inventory.")
         .defaultValue(false)
         .build()
     );
-    private final Setting<switchModes> switchMode = sgGeneral.add(new EnumSetting.Builder<switchModes>()
-        .name("Prefer")
-        .description("Where do we pick items from.")
-        .defaultValue(switchModes.Inventory)
+    private final Setting<ItemMode> itemMode = sgGeneral.add(new EnumSetting.Builder<ItemMode>()
+        .name("Item Mode")
+        .description("Which item should be held in offhand.")
+        .defaultValue(ItemMode.Totem)
         .build()
     );
-    private final Setting<itemModes> itemMode = sgGeneral.add(new EnumSetting.Builder<itemModes>()
-        .name("Item")
-        .description("")
-        .defaultValue(itemModes.Crystal)
+    private final Setting<GapMode> gapMode = sgGeneral.add(new EnumSetting.Builder<GapMode>()
+        .name("Gapple Mode")
+        .description("When should we hold golden apples.")
+        .defaultValue(GapMode.Both)
         .build()
     );
-    private final Setting<swordGapModes> swordGapple = sgGeneral.add(new EnumSetting.Builder<swordGapModes>()
-        .name("Sword Gapple")
-        .description("Holds gapples while swording")
-        .defaultValue(swordGapModes.Pressing)
+    private final Setting<SwordMode> swordMode = sgGeneral.add(new EnumSetting.Builder<SwordMode>()
+        .name("Sword Mode")
+        .description("When should we hold sword.")
+        .defaultValue(SwordMode.Pressed)
+        .visible(gapMode.get()::isSword)
+        .build()
+    );
+    private final Setting<Boolean> safeSword = sgGeneral.add(new BoolSetting.Builder()
+        .name("Safe Sword")
+        .description("Only sword gaps if you have enough health.")
+        .defaultValue(false)
+        .visible(gapMode.get()::isSword)
         .build()
     );
     private final Setting<Double> delay = sgGeneral.add(new DoubleSetting.Builder()
         .name("Delay")
         .description("Delay between switches.")
-        .defaultValue(0.05)
+        .defaultValue(0.1)
         .range(0, 1)
         .sliderRange(0, 1)
         .build()
     );
-    private final Setting<Boolean> safety = sgGeneral.add(new BoolSetting.Builder()
-        .name("Safety")
-        .description("Tries to prevent offhand fails by switching while in danger")
-        .defaultValue(true)
-        .build()
-    );
-    private final Setting<Integer> safetyHealth = sgGeneral.add(new IntSetting.Builder()
-        .name("Safety Health")
-        .description("Holds totem if you would have under this health after getting damage.")
-        .defaultValue(0)
-        .range(0, 36)
-        .sliderMax(36)
-        .build()
-    );
-    private final Setting<Integer> hp = sgGeneral.add(new IntSetting.Builder()
+
+    //   Health page
+    private final Setting<Integer> hp = sgHealth.add(new IntSetting.Builder()
         .name("Health")
-        .description("At what health value we will switch")
+        .description("Switches to totem when health is under this value.")
         .defaultValue(14)
         .range(0, 36)
         .sliderMax(36)
         .build()
     );
+    private final Setting<Boolean> safety = sgHealth.add(new BoolSetting.Builder()
+        .name("Safety")
+        .description("Tries to prevent offhand fails by switching while in danger.")
+        .defaultValue(true)
+        .build()
+    );
+    private final Setting<Integer> safetyHealth = sgHealth.add(new IntSetting.Builder()
+        .name("Safety Health")
+        .description("Holds totem if you would have under this health after possible damages.")
+        .defaultValue(0)
+        .range(0, 36)
+        .sliderMax(36)
+        .visible(safety::get)
+        .build()
+    );
 
     private double timer = 0;
+    private Item item = null;
+    private Suicide suicide = null;
+    private AutoCrystalRewrite autoCrystalRewrite = null;
+    private CrystalAura crystalAura = null;
+    private AutoMine autoMine = null;
+
+    private long lastTime = 0;
 
     @EventHandler(priority = EventPriority.HIGHEST)
     private void onRender(Render3DEvent event) {
-        timer = Math.min(delay.get(), timer + event.frameTime);
-        if (mc.player != null && mc.world != null) {
-            Item item = getItem();
-            if (!mc.player.getOffHandStack().getItem().equals(item) && item != null) {
-                if (onlyInInv.get() && !(mc.currentScreen instanceof InventoryScreen)) {
-                    return;
-                }
-                FindItemResult slot = find(item, switchMode.get().equals(switchModes.Hotbar));
-                slot = slot.count() > 0 ? slot : find(item, !switchMode.get().equals(switchModes.Hotbar));
-                if (slot.count() > 0 && timer >= delay.get()) {
-                    timer = 0;
-                    InvUtils.move().from(slot.slot()).toOffhand();
-                }
-            }
+        if (mc.player == null || mc.world == null) {return;}
+        timer -= (System.currentTimeMillis() - lastTime) / 1000d;
+        lastTime = System.currentTimeMillis();
+
+        if (suicide == null) {suicide = Modules.get().get(Suicide.class);}
+        if (autoCrystalRewrite == null) {autoCrystalRewrite = Modules.get().get(AutoCrystalRewrite.class);}
+        if (crystalAura == null) {crystalAura = Modules.get().get(CrystalAura.class);}
+        if (autoMine == null) {autoMine = Modules.get().get(AutoMine.class);}
+
+        item = getItem();
+        if (item != null) {
+            update();
         }
     }
 
+    private void update() {
+        if (timer > 0) {return;}
+        if (mc.player.getOffHandStack().getItem() == item) {return;}
+        if (onlyInInv.get() && !(mc.currentScreen instanceof InventoryScreen)) {return;}
+
+        int slot = getSlot(getPredicate(item));
+
+        InvUtils.move().from(slot).toOffhand();
+        timer = delay.get();
+    }
+
+    private Predicate<Item> getPredicate(Item item) {
+        if (item == Items.GOLDEN_APPLE) {return OLEPOSSUtils::isGapple;}
+        if (item == Items.RED_BED) {return OLEPOSSUtils::isBedItem;}
+
+        return item::equals;
+    }
+
     private Item getItem() {
-        if (mc.player != null) {
-            Suicide suicide = Modules.get().get(Suicide.class);
+        if (inDanger() && !suicide.isActive() && itemAvailable(itemStack -> itemStack.getItem() == Items.TOTEM_OF_UNDYING)) {
+            return Items.TOTEM_OF_UNDYING;
+        }
 
-            boolean crystalAvailable = InvUtils.find(itemStack -> itemStack.getItem().equals(Items.END_CRYSTAL)).count() > 0;
-            boolean totemAvailable = !(suicide.isActive() && suicide.offhand.get()) && InvUtils.find(itemStack -> itemStack.getItem().equals(Items.TOTEM_OF_UNDYING)).count() > 0;
-            boolean gapAvailable = InvUtils.find(itemStack -> itemStack.getItem().equals(Items.ENCHANTED_GOLDEN_APPLE)).count() > 0;
-            boolean shouldGap = (swordGapple.get().equals(swordGapModes.Always) && OLEPOSSUtils.isSword(mc.player.getMainHandStack().getItem())) ||
-                (swordGapple.get().equals(swordGapModes.Pressing) && OLEPOSSUtils.isSword(mc.player.getMainHandStack().getItem()) && mc.options.useKey.isPressed()) ||
-                (swordGapple.get().equals(swordGapModes.Smart) && OLEPOSSUtils.isSword(mc.player.getMainHandStack().getItem()) && mc.options.useKey.isPressed());
-            boolean firstAvailable = itemMode.get().equals(itemModes.Crystal) ? crystalAvailable : gapAvailable;
-            boolean secondAvailable = itemMode.get().equals(itemModes.Crystal) ? gapAvailable : crystalAvailable;
-
-            double health = mc.player.getHealth() + mc.player.getAbsorptionAmount();
-            boolean safe = health > hp.get() && isSafe(health);
-            if (shouldGap && gapAvailable && !swordGapple.get().equals(swordGapModes.Smart) ||
-                (swordGapple.get().equals(swordGapModes.Smart) && safe && gapAvailable && shouldGap)) {
-                return Items.ENCHANTED_GOLDEN_APPLE;
-            } else if (itemMode.get().equals(itemModes.Totem) && totemAvailable) {
-                return Items.TOTEM_OF_UNDYING;
-            } else {
-                if (!totemAvailable) {
-                    if (firstAvailable) {
-                        return itemMode.get().equals(itemModes.Crystal) ? Items.END_CRYSTAL : Items.ENCHANTED_GOLDEN_APPLE;
-                    } else if (secondAvailable) {
-                        return itemMode.get().equals(itemModes.Crystal) ? Items.ENCHANTED_GOLDEN_APPLE : Items.END_CRYSTAL;
-                    } else {
-                        return null;
+        if (mc.player.getMainHandStack().getItem() instanceof SwordItem && (!safeSword.get() || !inDanger())) {
+            if (gapMode.get().sword) {
+                switch (swordMode.get()) {
+                    case Always -> {
+                        return Items.GOLDEN_APPLE;
                     }
-                } else {
-                    if (safe) {
-                        if (firstAvailable) {
-                            return itemMode.get().equals(itemModes.Crystal) ? Items.END_CRYSTAL : Items.ENCHANTED_GOLDEN_APPLE;
-                        } else if (secondAvailable) {
-                            return itemMode.get().equals(itemModes.Crystal) ? Items.ENCHANTED_GOLDEN_APPLE : Items.END_CRYSTAL;
+                    case Pressed -> {
+                        if (mc.options.useKey.isPressed()) {
+                            return Items.GOLDEN_APPLE;
                         }
-                    } else {
-                        return Items.TOTEM_OF_UNDYING;
                     }
                 }
             }
+        }
+
+        switch (itemMode.get()) {
+            case Totem -> {
+                if (!suicide.isActive() && itemAvailable(itemStack -> itemStack.getItem() == Items.TOTEM_OF_UNDYING)) {
+                    return Items.TOTEM_OF_UNDYING;
+                }
+            }
+            case Crystal -> {
+                if (itemAvailable(itemStack -> itemStack.getItem() == Items.END_CRYSTAL)) {
+                    return Items.END_CRYSTAL;
+                }
+            }
+            case Gapple -> {
+                if (itemAvailable(OLEPOSSUtils::isGapple)) {
+                    return Items.GOLDEN_APPLE;
+                }
+            }
+            case Bed -> {
+                if (itemAvailable(itemStack -> itemStack.getItem() instanceof BedItem)) {
+                    return Items.RED_BED;
+                }
+            }
+        }
+
+        if (itemAvailable(OLEPOSSUtils::isGapple) &&
+            switch (gapMode.get()) {
+                case Sword -> false;
+                case LastOption -> true;
+                case Both -> false;
+                case Never -> false;
+            }) {
+            return Items.GOLDEN_APPLE;
         }
         return null;
     }
 
-    private FindItemResult find(Item i, boolean hotbar) {
-        return InvUtils.find(itemStack -> itemStack.getItem().equals(i), hotbar ? 0 : 9, hotbar ? 8 : mc.player.getInventory().size());
+    private boolean inDanger() {
+        double health = mc.player.getHealth();
+
+        return
+            health <= hp.get() ||
+            (safety.get() && health - PlayerUtils.possibleHealthReductions() <= safetyHealth.get());
+    }
+    private int getSlot(Predicate<Item> predicate) {
+        double amount = -1;
+        int slot = -1;
+
+        ItemStack s;
+        for (int i = 9; i < mc.player.getInventory().size() + 1; i++) {
+            s = mc.player.getInventory().getStack(i);
+
+            if (!predicate.test(s.getItem())) {continue;}
+
+            if (s.getCount() > amount) {
+                slot = i;
+                amount = s.getCount();
+            }
+        }
+        if (slot >= 0) {return slot;}
+
+        for (int i = 0; i < 9; i++) {
+            s = mc.player.getInventory().getStack(i);
+
+            if (!predicate.test(s.getItem())) {continue;}
+
+            if (s.getCount() > amount) {
+                slot = i;
+                amount = s.getCount();
+            }
+        }
+
+        return slot;
+    }
+    private boolean crystalModuleCheck() {
+        return autoMine.isActive() || autoCrystalRewrite.isActive() || crystalAura.isActive();
     }
 
-    private boolean isSafe(double playerHP) {
-        return !safety.get() || PlayerUtils.possibleHealthReductions() < playerHP - safetyHealth.get();
+    private boolean itemAvailable(Predicate<ItemStack> predicate) {
+        return InvUtils.find(predicate).found();
     }
 
-    public enum switchModes {
-        Hotbar,
-        Inventory,
-    }
-
-    public enum swordGapModes {
-        Disabled,
-        Always,
-        Pressing,
-        Smart
-    }
-
-    public enum itemModes {
+    public enum ItemMode {
+        Totem,
         Crystal,
         Gapple,
-        Totem
+        Bed
+    }
+    public enum GapMode {
+        Sword(true),
+        LastOption(false),
+        Both(true),
+        Never(false);
+
+        public final boolean sword;
+        GapMode(boolean sword) {
+            this.sword = sword;
+        }
+        public boolean isSword() {
+            return sword;
+        }
+    }
+    public enum SwordMode {
+        Pressed,
+        Always
     }
 }
