e72cc1674f4af9fa3b98f42485da9dd20ac848b8
OLEPOSSU <atska07@gmail.com>
2023-06-30 16:11:29 +0300

    fixed step+ sending packets even when disabled

--- src/main/java/kassuk/addon/blackout/mixins/MixinEntity.java
+++ src/main/java/kassuk/addon/blackout/mixins/MixinEntity.java
@@ -65,6 +65,7 @@ public abstract class MixinEntity {
     @Inject(method = "adjustMovementForCollisions(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;", at = @At("HEAD"), cancellable = true)
     private void inject(Vec3d movement, CallbackInfoReturnable<Vec3d> cir) {
         StepPlus step = Modules.get().get(StepPlus.class);
+
         Entity entity = (Entity)(Object)this;
 
         if (step.isActive() && step.slow.get()) {
@@ -91,88 +92,8 @@ public abstract class MixinEntity {
 
             if (vec3d2.horizontalLengthSquared() > vec3d.horizontalLengthSquared()) {
                 Vec3d v = vec3d2.add(Entity.adjustMovementForCollisions(entity, new Vec3d(0.0, -vec3d2.y + movement.y, 0.0), box.offset(vec3d2), entity.getWorld(), list));
-                step.step(step.getOffsets(v.y));
+                if (step.isActive()) step.step(step.getOffsets(v.y));
                 cir.setReturnValue(v);
-                return;
-            }
-        }
-
-        cir.setReturnValue(vec3d);
-    }
-
-    private void i(Vec3d movement, CallbackInfoReturnable<Vec3d> cir) {
-        Entity entity = ((Entity)(Object) this);
-        StepPlus step = Modules.get().get(StepPlus.class);
-
-        if (step.isActive()) {
-            // Step
-
-            Box box = this.getBoundingBox();
-            List<VoxelShape> list = this.getWorld().getEntityCollisions(entity, box.stretch(movement));
-            Vec3d vec3d = movement.lengthSquared() == 0.0 ? movement : Entity.adjustMovementForCollisions(entity, movement, box, this.getWorld(), list);
-            boolean bl = movement.x != vec3d.x;
-            boolean bl3 = movement.z != vec3d.z;
-            boolean bl4 = this.isOnGround() || (step.slow.get() && step.stepping);
-            if (bl4 && (bl || bl3)) {
-                Vec3d vec3d2 = Entity.adjustMovementForCollisions(entity, new Vec3d(movement.x, step.stepping && step.currentOffsets != null ? step.currentOffsets[step.index] : step.height.get(), movement.z), box, this.getWorld(), list);
-                Vec3d vec3d3 = Entity.adjustMovementForCollisions(entity, new Vec3d(0.0, step.stepping && step.currentOffsets != null ? step.currentOffsets[step.index] : step.height.get(), 0.0), box.stretch(movement.x, 0.0, movement.z), this.getWorld(), list);
-                if (vec3d3.y < step.height.get() || step.stepping) {
-                    Vec3d vec3d4 = Entity.adjustMovementForCollisions(entity, new Vec3d(movement.x, 0.0, movement.z), box.offset(vec3d3), this.getWorld(), list).add(vec3d3);
-                    if (vec3d4.horizontalLengthSquared() > vec3d2.horizontalLengthSquared() || step.stepping) {
-                        if (step.slow.get() && !step.stepping) {
-                            step.currentOffsets = step.getOffsets(vec3d3.y);
-
-                            if (step.currentOffsets != null) {
-                                step.stepping = true;
-                                step.index = 0;
-                            }
-                        }
-                        vec3d2 = vec3d4;
-                    }
-                }
-
-                if (vec3d2.horizontalLengthSquared() > vec3d.horizontalLengthSquared() || (step.stepping && step.currentOffsets != null)) {
-                    Vec3d m = vec3d2.add(Entity.adjustMovementForCollisions(entity, new Vec3d(0.0, -vec3d2.y + movement.y, 0.0), box.offset(vec3d2), this.getWorld(), list));
-
-                    if (!step.slow.get()) {
-                        step.step(step.getOffsets(m.y));
-                        cir.setReturnValue(m);
-                        return;
-                    } else {
-                        ((IVec3d) m).setY(step.currentOffsets[step.index]);
-                        if (step.index >= step.currentOffsets.length) {
-                            step.stepping = false;
-                        }
-                        cir.setReturnValue(m);
-                        return;
-                    }
-                }
-            }
-
-            cir.setReturnValue(vec3d);
-            return;
-        }
-
-        // Vanilla
-        Box box = this.getBoundingBox();
-        List<VoxelShape> list = this.getWorld().getEntityCollisions(entity, box.stretch(movement));
-        Vec3d vec3d = movement.lengthSquared() == 0.0 ? movement : Entity.adjustMovementForCollisions(entity, movement, box, this.getWorld(), list);
-        boolean bl = movement.x != vec3d.x;
-        boolean bl2 = movement.y != vec3d.y;
-        boolean bl3 = movement.z != vec3d.z;
-        boolean bl4 = this.isOnGround() || bl2 && movement.y < 0.0;
-        if (getStepHeight() > 0.0F && bl4 && (bl || bl3)) {
-            Vec3d vec3d2 = Entity.adjustMovementForCollisions(entity, new Vec3d(movement.x, getStepHeight(), movement.z), box, this.getWorld(), list);
-            Vec3d vec3d3 = Entity.adjustMovementForCollisions(entity, new Vec3d(0.0, getStepHeight(), 0.0), box.stretch(movement.x, 0.0, movement.z), this.getWorld(), list);
-            if (vec3d3.y < getStepHeight()) {
-                Vec3d vec3d4 = Entity.adjustMovementForCollisions(entity, new Vec3d(movement.x, 0.0, movement.z), box.offset(vec3d3), this.getWorld(), list).add(vec3d3);
-                if (vec3d4.horizontalLengthSquared() > vec3d2.horizontalLengthSquared()) {
-                    vec3d2 = vec3d4;
-                }
-            }
-
-            if (vec3d2.horizontalLengthSquared() > vec3d.horizontalLengthSquared()) {
-                cir.setReturnValue(vec3d2.add(Entity.adjustMovementForCollisions(entity, new Vec3d(0.0, -vec3d2.y + movement.y, 0.0), box.offset(vec3d2), this.getWorld(), list)));
                 return;
             }
         }
